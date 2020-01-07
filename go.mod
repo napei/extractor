7bd695cc742f368c288738834ea428800282cee0
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2020-01-07 22:31:02 +1100

    Add Code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/napei/extractor
+
+go 1.13
+
+require (
+	github.com/fatih/color v1.8.0
+	github.com/mholt/archiver/v3 v3.3.0
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/mholt/archiver/v3"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var files []string
+var outputdir string
+
+func searchForArchives(rootpath string) error {
+	var partRegex = regexp.MustCompile("^.*(part[0-9]+\\.rar)$")
+
+	fmt.Printf(color.YellowString("[Looking for Archives]\n"))
+	err := filepath.Walk(rootpath, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && (strings.Contains(info.Name(), "part01.rar") || (!partRegex.MatchString(info.Name()) && strings.Contains(info.Name(), ".rar"))) {
+			fmt.Printf(color.GreenString("[Found Archive]: ") + path + "\n")
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	return err
+}
+
+func extractArchives() {
+	fmt.Printf(color.YellowString("\n[Extracting Archives]\n"))
+	for i := range files {
+		outputpath := filepath.Dir(files[i])
+		fmt.Printf(color.GreenString("[Extracting Archive]: ") + files[i] + color.BlueString(" - to directory ["+filepath.Dir(files[i])+"]\n"))
+		archiver.Unarchive(files[i], outputpath)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+
+	// Handle Args
+	if !(len(os.Args) > 1) {
+		fmt.Printf(color.RedString("ERROR: input path not specified. Call the program as: ") + filepath.Base(os.Args[0]) + " [input path] <flags>\n")
+		fmt.Printf("For help, use the '-h' flag\n")
+		os.Exit(1)
+	}
+	flag.StringVar(&outputdir, "output", "", "Specify an alternate output directory for all located archives. By default, this program will output archives in the same folder.")
+	flag.Parse()
+
+	searchForArchives(args[0])
+	extractArchives()
+}
